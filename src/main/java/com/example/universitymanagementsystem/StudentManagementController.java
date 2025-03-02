3956507d123cb4cc57ba0264de2a9799f1495d87
Jkhaleed <khaleedjimoh05@gmail.com>
2025-03-02 16:07:54 -0500

    Planning on working on the Student management control

---
+++ src/main/java/com/example/universitymanagementsystem/StudentManagementController.java
@@ -0,0 +1,8 @@
+package com.example.universitymanagementsystem;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+
+public class StudentManagementController {
+}

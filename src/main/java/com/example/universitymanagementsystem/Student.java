27bc72ea0d03f10736f1b2bfb059a9c42c9db606
Jkhaleed <khaleedjimoh05@gmail.com>
2025-03-03 15:06:35 -0500

    Initialize

---
+++ src/main/java/com/example/universitymanagementsystem/Student.java
@@ -0,0 +1,65 @@
+package com.example.universitymanagementsystem;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+public class Student {
+    private final StringProperty studentId;
+    private final StringProperty name;
+    private final StringProperty email;
+    private final StringProperty academicLevel;
+
+    public Student(String studentId, String name, String email, String academicLevel){
+        this.studentId = new SimpleStringProperty(studentId);
+        this.name = new SimpleStringProperty(name);
+        this.email = new SimpleStringProperty(email);
+        this.academicLevel = new SimpleStringProperty(academicLevel);
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId.set(studentId);
+    }
+
+    public String getStudentId() {
+        return studentId.get();
+    }
+
+    public StringProperty studentIdProperty() {
+        return studentId;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public StringProperty emailProperty() {
+        return email;
+    }
+
+    public void setAcademicLevel(String academicLevel) {
+        this.academicLevel.set(academicLevel);
+    }
+
+    public String getAcademicLevel() {
+        return academicLevel.get();
+    }
+
+    public StringProperty academicLevelProperty() {
+        return academicLevel;
+    }
+}
